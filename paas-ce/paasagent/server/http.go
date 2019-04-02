ba90359b7dc1827a47e6f7c1fcab2ef98b37dde0
jamesge <gejun.coolfriend@gmail.com>
2019-04-02 18:30:18 +0800

    add annotation for paasagent (#57)

--- paas-ce/paasagent/server/http.go
+++ paas-ce/paasagent/server/http.go
@@ -27,7 +27,7 @@ import (
 	"paasagent/job"
 )
 
-// DeployData be used
+// DeployData used for app deployment
 type DeployData struct {
 	AppCode      string                 `json:"app_code"`
 	Mode         string                 `json:"mode"`
@@ -138,6 +138,7 @@ func Run() error {
 	e.POST("/v1/app/online", appOnlineHandler)
 	e.POST("/v1/app/offline", appOfflineHandler)
 	e.GET("/v1/app/healthz", healthzHandler)
+	// check if paasagent is alive
 	e.GET("/healthz", healthzCheckHandler)
 
 	port := viper.GetInt("port")
