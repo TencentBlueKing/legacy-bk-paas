adc0998b513ae49a891be659ed771e4147558a34
xuzhan <xuzhan@tencent.com>
2020-07-10 17:53:36 +0800

    添加权限相关方法 (#240)
    * 添加快捷操作功能
    
    * 拖拽快捷键支持Mac Command键
    
    * 修改快捷操作说明
    
    * 修复剪切快捷操作，修改快捷操作说明
    
    * 兼容数据为空的情况
    
    * 兼容数据为空的情况
    
    * 仅在选中拖拽区域触发快捷操作
    
    * 修改判断是否在拖拽区域
    
    * 增加backspace快速删除
    修改快捷面版样式
    
    * 后端采用typeorm
    
    * orm添加migration
    
    * 修改文件名
    
    * 修改数据迁移表
    
    * 1. 新增数据库部署文档
    2. 去掉test相关接口
    3. 修改initial.sql注释
    
    * 去掉数据迁移部分，采用SQL自己控制
    
    * 1. 修改数据库说明
    2. 修改数据库配置注释
    
    * 1. entity的base 添加创建人和修改人字段
    2. 添加函数相关的entities
    3. 修改initial.sql
    
    * 修改字段类型
    
    * 1. 添加权限相关的实体和接口
    2. 添加创建人和更新人字段
    
    Co-authored-by: ielgnaw <wuji0223@gmail.com>

--- paas-ce/lesscode/lib/server/model/entities/project-func-group.js
+++ paas-ce/lesscode/lib/server/model/entities/project-func-group.js
@@ -1,19 +1,24 @@
-import { Entity, PrimaryGeneratedColumn, Column } from 'typeorm'
+/**
+ * Tencent is pleased to support the open source community by making 蓝鲸智云PaaS平台社区版 (BlueKing PaaS Community Edition) available.
+ * Copyright (C) 2017-2018 THL A29 Limited, a Tencent company. All rights reserved.
+ * Licensed under the MIT License (the "License"); you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * http://opensource.org/licenses/MIT
+ * Unless required by applicable law or agreed to in writing, software distributed under the License is distributed on
+ * an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License for the
+ * specific language governing permissions and limitations under the License.
+ */
 
-@Entity({ name: 'r_project_func_group', comment: '项目/函数关联表' })
-export default class ProjectComp {
-    @PrimaryGeneratedColumn()
-    id
+import { Entity, Column } from "typeorm";
+import base from './base'
 
-    @Column({
-        type: 'int',
-        comment: 'project 表主键'
-    })
+@Entity()
+export class r_project_func_group extends base {
+    // project 表主键
+    @Column({ type: "int" })
     projectId
 
-    @Column({
-        type: 'int',
-        comment: 'function 表主键'
-    })
+    // function 表主键
+    @Column({ type: "int" })
     funcGroupId
 }
