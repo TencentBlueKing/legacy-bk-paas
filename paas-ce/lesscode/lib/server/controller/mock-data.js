e90931a05cf5096cd2bdc87d5744ae85e70c8a7e
ielgnaw <wuji0223@gmail.com>
2020-08-04 09:41:20 +0800

    登录权限控制 & 账号管理页面 & 用户信息 (#259)

--- paas-ce/lesscode/lib/server/controller/mock-data.js
+++ paas-ce/lesscode/lib/server/controller/mock-data.js
@@ -8,8 +8,9 @@
  * an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License for the
  * specific language governing permissions and limitations under the License.
  */
-function strToJson(str){
-    var json = (new Function("return " + str))()
+function strToJson (str) {
+    // eslint-disable-next-line no-new-func
+    const json = (new Function('return ' + str))()
     return json
 }
 
