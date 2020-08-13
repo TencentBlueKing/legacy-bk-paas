949b46afccb51e993e23ad2a6ebcb0832a573489
ielgnaw <wuji0223@gmail.com>
2020-08-13 16:33:30 +0800

    修复属性面板中 switch 组件不会正确渲染的问题

--- paas-ce/lesscode/lib/client/src/element-materials/modifier/props/components/strategy/boolean.vue
+++ paas-ce/lesscode/lib/client/src/element-materials/modifier/props/components/strategy/boolean.vue
@@ -9,6 +9,44 @@
   specific language governing permissions and limitations under the License.
 -->
 
-<template functional>
-    <bk-switcher theme="primary" size="large" :value="props.defaultValue" @change="val => props.change(`${props.name}`, val, props.type)" />
+<template>
+    <div class="modifier-props-switch-container">
+        <!-- <bk-switcher theme="primary" size="large" :value="props.defaultValue" @change="val => props.change(`${props.name}`, val, props.type)" /> -->
+        <bk-switcher theme="primary" size="large" :value="renderValue" @change="handleChange" />
+    </div>
 </template>
+
+<script>
+    export default {
+        props: {
+            defaultValue: {
+                type: Boolean,
+                required: true
+            },
+            name: {
+                type: String,
+                required: true
+            },
+            type: {
+                type: String,
+                required: true
+            },
+            change: {
+                type: Function,
+                required: true
+            }
+        },
+        data () {
+            return {
+                isError: false,
+                renderValue: this.defaultValue
+            }
+        },
+        methods: {
+            handleChange (val) {
+                this.renderValue = !this.renderValue
+                this.change(`${this.name}`, val, this.type)
+            }
+        }
+    }
+</script>
