146fa8721c96b460886bd8af3eb08abd08a7e2de
terlinhe <terlinhe@tencent.com>
2021-05-25 11:47:01 +0800

    fix: delete图标库相关code

--- paas-ce/lesscode/lib/client/src/element-materials/modifier/template/editor/menu/edit.vue
+++ paas-ce/lesscode/lib/client/src/element-materials/modifier/template/editor/menu/edit.vue
@@ -2,14 +2,11 @@
     <div class="template-menu-edit">
         <div class="menu-name">
             <bk-input placeholder="请输入导航名称" :value="baseInfo.name" @change="handleNameChange">
-                <div
-                    v-if="showIcon"
-                    slot="prepend"
-                    ref="icon"
-                    class="group-text"
-                    style="padding: 0 10px">
-                    <i :class="baseInfo.icon" />
-                </div>
+                <template v-if="showIcon" slot="prepend">
+                    <div class="group-text" style="padding: 0 10px">
+                        <i class="bk-icon icon-block-shape" />
+                    </div>
+                </template>
             </bk-input>
         </div>
         <div class="menu-page-wraper">
@@ -65,68 +62,10 @@
                 <div class="text">{{ isPageCode ? '路由' : '链接' }}</div>
             </div>
         </div>
-        <div style="display: none">
-            <div ref="iconPanel" class="template-icon-custom-panel">
-                <div v-if="isIconListRender">
-                    <div class="list-icon-search">
-                        <input
-                            ref="search"
-                            spellcheck="false"
-                            placeholder="输入 icon 的名字"
-                            @input="handleInputChange">
-                        <i class="bk-icon icon-search icon-search-flag" />
-                    </div>
-                    <div class="wraper">
-                        <template v-if="searchValue">
-                            <div
-                                class="item"
-                                v-for="searchItem in searchList"
-                                :key="searchItem.icon"
-                                @click="handleIconChange(searchItem.icon)">
-                                <i :class="searchItem.icon" />
-                                <span class="item-name">{{ searchItem.name }}</span>
-                            </div>
-                            <div v-if="searchList.length < 1" key="searchEmpty" class="search-empty">暂无数据</div>
-                        </template>
-                        <template v-else>
-                            <div v-if="iconList.length > 0" class="group">
-                                <div class="group-name">自定义图标（{{ iconList.length }}）</div>
-                                <div
-                                    v-for="iconItem in iconList"
-                                    class="item"
-                                    :key="iconItem.icon"
-                                    @click="handleIconChange(iconItem.icon)">
-                                    <i :class="iconItem.icon" />
-                                    <span class="item-name">{{ iconItem.name }}</span>
-                                </div>
-                            </div>
-                            <template v-for="buildIconGroupName in Object.keys(buildInIconGroup)">
-                                <div
-                                    v-if="buildInIconGroup[buildIconGroupName].length > 0"
-                                    class="group"
-                                    :key="buildIconGroupName">
-                                    <div class="group-name">{{ buildIconGroupName }}（{{ buildInIconGroup[buildIconGroupName].length }}）</div>
-                                    <div
-                                        v-for="iconItem in buildInIconGroup[buildIconGroupName]"
-                                        class="item"
-                                        :key="iconItem.icon"
-                                        @click="handleIconChange(iconItem.icon)">
-                                        <i :class="iconItem.icon" />
-                                        <span class="item-name">{{ iconItem.name }}</span>
-                                    </div>
-                                </div>
-                            </template>
-                        </template>
-                    </div>
-                </div>
-            </div>
-        </div>
     </div>
 </template>
 <script>
-    import _ from 'lodash'
     import { mapState } from 'vuex'
-    import iconComponentList from '@/element-materials/materials/icon-list.js'
 
     export default {
         name: '',
@@ -145,21 +84,16 @@
                 isPageCode: true,
                 isShowPageQuery: false,
                 baseInfo: {
-                    icon: '',
                     name: '',
                     pageCode: '',
                     fullPath: '',
                     link: '',
                     query: ''
-                },
-                searchValue: '',
-                searchList: [],
-                isIconListRender: false
+                }
             }
         },
         computed: {
             ...mapState('route', ['layoutPageList']),
-            ...mapState('iconManage', ['iconList']),
             pageRouteList () {
                 const pageRouteList = this.layoutPageList.map(item => {
                     const { id, layoutPath, path } = item
@@ -174,22 +108,7 @@
                 return pageRouteList
             }
         },
-        mounted () {
-            this.$nextTick(() => {
-                this.initPopover()
-            })
-        },
         created () {
-            this.buildInIconGroup = iconComponentList.reduce((result, item) => {
-                if (!result[item.group]) {
-                    result[item.group] = []
-                }
-                result[item.group].push({
-                    icon: `bk-icon ${item.name}`,
-                    name: item.icon
-                })
-                return result
-            }, {})
             this.baseInfo = { ...this.data }
             this.isPageCode = !this.data.link
             this.isShowPageQuery = !!this.data.query
@@ -199,62 +118,6 @@
                 this.$emit('on-change', {
                     ...this.baseInfo
                 })
-            },
-            initPopover () {
-                if (!this.popperInstance && this.showIcon) {
-                    this.popperInstance = this.$bkPopover(this.$refs.icon, {
-                        theme: 'light template-custom-icon',
-                        arrow: false,
-                        interactive: true,
-                        animateFill: false,
-                        placement: 'bottom-start',
-                        content: this.$refs.iconPanel,
-                        trigger: 'click',
-                        distance: 20,
-                        width: '276px',
-                        size: 'small',
-                        zIndex: window.__bk_zIndex_manager.nextZIndex(),
-                        onShow: () => {
-                            this.isIconListRender = true
-                        },
-                        onHidden: () => {
-                            this.isIconListRender = false
-                        }
-                    })
-                    this.$once('hook:beforeDestroy', () => {
-                        this.popperInstance.destroy()
-                    })
-                }
-            },
-            handleInputChange (event) {
-                const localSearch = _.trim(event.target.value).toLocaleLowerCase()
-                this.searchValue = localSearch
-                if (!localSearch) {
-                    this.searchList = []
-                    return
-                }
-                const result = []
-                for (let i = 0; i < this.iconList.length; i++) {
-                    const curItem = this.iconList[i]
-                    if (curItem.name.indexOf(localSearch) > -1) {
-                        result.push(curItem)
-                    }
-                }
-                for (let i = 0; i < iconComponentList.length; i++) {
-                    const curItem = iconComponentList[i]
-                    if (curItem.icon.indexOf(localSearch) > -1) {
-                        result.push({
-                            icon: `bk-icon ${curItem.name}`,
-                            name: curItem.icon
-                        })
-                    }
-                }
-                this.searchList = Object.freeze(result)
-            },
-            handleIconChange (icon) {
-                this.baseInfo.icon = icon
-                this.popperInstance.hide()
-                this.triggerChange()
             },
             handleNameChange (name) {
                 this.baseInfo.name = name
