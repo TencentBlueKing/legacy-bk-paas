301e298462edd03dcadb7fe4322cbbe6fb2cfa4b
terlinhe <terlinhe@tencent.com>
2021-06-13 10:51:18 +0800

    feat: 增加数据库表索引

--- paas-ce/lesscode/lib/client/src/element-materials/materials/bk/input/index.js
+++ paas-ce/lesscode/lib/client/src/element-materials/materials/bk/input/index.js
@@ -54,7 +54,7 @@ export default {
     props: {
         value: {
             type: 'string',
-            val: 'hello world'
+            val: ''
         },
         type: {
             type: 'string',
