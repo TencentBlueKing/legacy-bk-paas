a446ce740b7276483064b4f5f068766af05587d6
terlinhe <terlinhe@tencent.com>
2021-09-26 12:11:09 +0800

    fix: fix模板文案、去除产品分类

--- paas-ce/lesscode/lib/client/src/views/project/template-manage/index.vue
+++ paas-ce/lesscode/lib/client/src/views/project/template-manage/index.vue
@@ -1,6 +1,6 @@
 <template>
     <section>
-        <bk-alert type="info" title="模板来源：在画布中可将局部内容存为模版或将整个页面存为模版" closable></bk-alert>
+        <bk-alert type="info" title="模板来源：在画布中可将局部内容存为模板或将整个页面存为模板" closable></bk-alert>
         <layout class="template-manage" :init-width="300">
             <render-category ref="category" slot="left" @on-change="handleCategoryChange" />
             <render-template v-if="categoryId" :category-id="categoryId" @on-change="handleComponentChange" />
