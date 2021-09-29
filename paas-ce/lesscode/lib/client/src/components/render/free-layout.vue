dc9e33a0a2c98fa632c8f0408f73eb7f69315a1c
vincenttgao <vincenttgao@tencent.com>
2021-09-29 11:13:18 +0800

    bugfix: 修复右键菜单无法触发的问题

--- paas-ce/lesscode/lib/client/src/components/render/free-layout.vue
+++ paas-ce/lesscode/lib/client/src/components/render/free-layout.vue
@@ -318,7 +318,7 @@
                 const curRowNode = getNodeWithClass(e.target, 'bk-lesscode-free-layout')
                 curRowNode.classList.add('selected')
 
-                this.$clearMenu()
+                bus.$emit('hideContextMenu') // 隐藏右键菜单()
                 this.setCurSelectedComponentData(_.cloneDeep(this.renderData))
                 bus.$emit('selected-tree', this.renderData.componentId)
             },
