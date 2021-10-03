39714f9f1d908cb303c96548010237028dc7278d
poloohuang <poloohuang@tencent.com>
2021-10-03 20:14:50 +0800

    fix: 修复圆角设置属性的问题

--- paas-ce/lesscode/lib/client/src/element-materials/modifier/component/styles/strategy/border.vue
+++ paas-ce/lesscode/lib/client/src/element-materials/modifier/component/styles/strategy/border.vue
@@ -176,7 +176,8 @@
                 borderTopStyle: this.value.borderTopStyle || '',
                 borderRightStyle: this.value.borderRightStyle || '',
                 borderBottomStyle: this.value.borderBottomStyle || '',
-                borderLeftStyle: this.value.borderLeftStyle || ''
+                borderLeftStyle: this.value.borderLeftStyle || '',
+                borderList: ['borderTop', 'borderRight', 'borderBottom', 'borderLeft']
             }
         },
         watch: {
@@ -219,7 +220,9 @@
                 this[key] = val
                 this.change(key, val)
                 if (pattern === 'border') {
-                    this.clearSeparateData('Style')
+                    for (const e of this.borderList) {
+                        this.change(e + 'Style', val)
+                    }
                 }
             },
             handleBorderWidthChange (pattern, val) {
@@ -228,7 +231,9 @@
                 const newVal = val === '' ? '' : val + 'px'
                 this.change(key, newVal)
                 if (pattern === 'border') {
-                    this.clearSeparateData('Width')
+                    for (const e of this.borderList) {
+                        this.change(e + 'Width', val)
+                    }
                 }
             },
             handleBorderColorChange (pattern, val) {
@@ -236,7 +241,9 @@
                 this[key] = val
                 this.change(key, val)
                 if (pattern === 'border') {
-                    this.clearSeparateData('Color')
+                    for (const e of this.borderList) {
+                        this.change(e + 'Color', val)
+                    }
                 }
             },
             // 修改 border 简写 css 时，清除其他四个方向的 css
