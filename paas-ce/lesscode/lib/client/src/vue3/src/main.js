900e917bf86e0989ea9715b3f24e9fa52819384e
ielgnaw <wuji0223@gmail.com>
2021-10-21 20:47:15 +0800

    feat: update

--- paas-ce/lesscode/lib/client/src/vue3/src/main.js
+++ paas-ce/lesscode/lib/client/src/vue3/src/main.js
@@ -12,11 +12,11 @@ app.mount('#vue3-app')
 
 window.globalStr = 'vue3'
 
-console.log('微应用vue3渲染了')
+console.log('vue3 中的 window.globalStr', window.globalStr)
 
 // 监听卸载
 window.addEventListener('unmount', function () {
-  console.log('微应用vue3卸载了')
+  console.log('vue3卸载了')
   // 卸载应用
   app.unmount()
 })
--- paas-ce/lesscode/lib/client/src/vue3/src/public-path.js
+++ paas-ce/lesscode/lib/client/src/vue3/src/public-path.js
@@ -1,5 +1,5 @@
 /* eslint-disable no-undef */
 if (window.__MICRO_APP_ENVIRONMENT__) {
-  console.log('window.__MICRO_APP_PUBLIC_PATH__', window.__MICRO_APP_PUBLIC_PATH__)
+  // console.log('window.__MICRO_APP_PUBLIC_PATH__', window.__MICRO_APP_PUBLIC_PATH__)
   __webpack_public_path__ = window.__MICRO_APP_PUBLIC_PATH__
 }
