58a4a63e17c8de2a7ff86f77681920882596d317
ielgnaw <wuji0223@gmail.com>
2021-10-26 22:20:43 +0800

    feat: comment

--- paas-ce/lesscode/lib/client/src/lesscode-canvas/index.js
+++ paas-ce/lesscode/lib/client/src/lesscode-canvas/index.js
@@ -16,23 +16,32 @@ class LesscodeCanvas extends HTMLElement {
         return ['name', 'host', 'path', 'route']
     }
 
-    // 在 custom element 增加、删除或者修改某个属性时被调用。
+    /**
+     * 在 custom element 增加、删除或者修改某个属性时被调用
+     *
+     * @param {string} attrName 变化的属性
+     * @param {string} oldValue 变化前的值
+     * @param {string} newValue 变化后的值
+     *
+     * name: 子应用名字
+     * host: 子应用的 host
+     *      dev 时开启另一个端口，便于 hotreload
+     *      生产环境时为空，使用当前父应用的 host，为了满足生产环境不便于启动多端口的情况
+     * path: 拉取子应用入口 html 的 path。
+     *      子应用入口 html 完整路径为 host + path。同时此 path 还要适配子应用中动态插入的静态资源的路径
+     * route: 子应用的 base route，会挂载到 window.__MICRO_APP_BASE_ROUTE__ 便于子应用获取
+     */
     attributeChangedCallback (attrName, oldValue, newValue) {
-        // console.warn(`attrName: ${attrName} --- oldValue: ${oldValue} --- newValue: ${newValue}`)
+        console.error('-------- attributeChangedCallback --------')
+        console.warn(`attrName: ${attrName} --- oldValue: ${oldValue} --- newValue: ${newValue}`)
         if (newValue) {
             this[attrName] = newValue
         }
-        // if (attrName === 'name' && !this.name && newValue) {
-        //     this.name = newValue
-        // } else if (attrName === 'entry' && !this.entry && newValue) {
-        //     this.entry = newValue
-        // } else if (attrName === 'route' && !this.route && newValue) {
-        //     this.route = newValue
-        // }
     }
 
     // 当 custom element 首次被插入文档 DOM 时，被调用
     connectedCallback () {
+        console.error('-------- connectedCallback --------')
         // console.log('connectedCallbackconnectedCallbackconnectedCallback')
         this.entry = this.host.replace(/\/$/, '') + '/' + this.path.replace(/^\//, '')
         console.log(this.name, this.host, this.path, this.route, this.entry)
@@ -48,12 +57,12 @@ class LesscodeCanvas extends HTMLElement {
 
     // 当 custom element 被移动到新的文档时，被调用
     adoptedCallback () {
-        console.warn('adoptedCallback')
+        console.warn('-------- adoptedCallback --------')
     }
 
     // 当 custom element 从文档 DOM 中删除时，被调用
     disconnectedCallback () {
-        console.warn('disconnectedCallback')
+        console.warn('-------- disconnectedCallback --------')
     }
 }
 
--- paas-ce/lesscode/lib/client/src/vue3/vue.config.js
+++ paas-ce/lesscode/lib/client/src/vue3/vue.config.js
@@ -1,3 +1,15 @@
+// <lesscode-canvas> 的属性 host, path
+// host + path
+
+// dev
+// 能获取到 html，这里 path 只要是 static 开头就可以了，static 后面的路径无所谓，dev 时都能获取到，如 static/dist-vueadsasdas3asddasdsaasddas, static/asddasads
+// 注意这里获取的是静态的，由 webpack 动态插入的是不行的。
+// path 的配置与 vue.config.js 里的 publicPath 配置一致时，webpack 动态插入才正确
+
+
+// build
+// path 配置为 static/${outputDir} 时才能生效
+
 module.exports = {
   // dev 时的 path 路径，以及 build 时 html 中引入静态资源的 path
   publicPath: '/static/dist-vue3/',
