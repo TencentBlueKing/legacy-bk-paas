aa2ddd89cfa60c9fe0b6c444e0903055a23b81d2
ielgnaw <wuji0223@gmail.com>
2021-11-03 15:10:11 +0800

    feat: 数据源管理

--- paas-ce/lesscode/lib/server/model/migrations/20211014084206-update-sql.js
+++ paas-ce/lesscode/lib/server/model/migrations/20211014084206-update-sql.js
@@ -1,53 +1,53 @@
-'use strict'
+'use strict';
 
-let dbm
-let type
-let seed
-const fs = require('fs')
-const path = require('path')
-let Promise
+var dbm;
+var type;
+var seed;
+var fs = require('fs');
+var path = require('path');
+var Promise;
 
 /**
   * We receive the dbmigrate dependency from dbmigrate initially.
   * This enables us to not have to rely on NODE_PATH.
   */
-exports.setup = function (options, seedLink) {
-    dbm = options.dbmigrate
-    type = dbm.dataType
-    seed = seedLink
-    Promise = options.Promise
-}
+exports.setup = function(options, seedLink) {
+  dbm = options.dbmigrate;
+  type = dbm.dataType;
+  seed = seedLink;
+  Promise = options.Promise;
+};
 
-exports.up = function (db) {
-    const filePath = path.join(__dirname, 'sqls', '20211014084206-update-sql-up.sql')
-    return new Promise(function (resolve, reject) {
-        fs.readFile(filePath, { encoding: 'utf-8' }, function (err, data) {
-            if (err) return reject(err)
-            console.log('received data: ' + data)
+exports.up = function(db) {
+  var filePath = path.join(__dirname, 'sqls', '20211014084206-update-sql-up.sql');
+  return new Promise( function( resolve, reject ) {
+    fs.readFile(filePath, {encoding: 'utf-8'}, function(err,data){
+      if (err) return reject(err);
+      console.log('received data: ' + data);
 
-            resolve(data)
-        })
-    })
-        .then(function (data) {
-            return db.runSql(data)
-        })
-}
+      resolve(data);
+    });
+  })
+  .then(function(data) {
+    return db.runSql(data);
+  });
+};
 
-exports.down = function (db) {
-    const filePath = path.join(__dirname, 'sqls', '20211014084206-update-sql-down.sql')
-    return new Promise(function (resolve, reject) {
-        fs.readFile(filePath, { encoding: 'utf-8' }, function (err, data) {
-            if (err) return reject(err)
-            console.log('received data: ' + data)
+exports.down = function(db) {
+  var filePath = path.join(__dirname, 'sqls', '20211014084206-update-sql-down.sql');
+  return new Promise( function( resolve, reject ) {
+    fs.readFile(filePath, {encoding: 'utf-8'}, function(err,data){
+      if (err) return reject(err);
+      console.log('received data: ' + data);
 
-            resolve(data)
-        })
-    })
-        .then(function (data) {
-            return db.runSql(data)
-        })
-}
+      resolve(data);
+    });
+  })
+  .then(function(data) {
+    return db.runSql(data);
+  });
+};
 
 exports._meta = {
-    'version': 1
-}
+  "version": 1
+};
