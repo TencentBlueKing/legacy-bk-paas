4913dddcd7827338d7ab57ee69442cbc48103e85
terlinhe <terlinhe@tencent.com>
2022-03-22 16:18:52 +0800

    fix: enter快捷键不生效问题

--- paas-ce/lesscode/lib/client/src/element-materials/core/helper/commands/remove.js
+++ paas-ce/lesscode/lib/client/src/element-materials/core/helper/commands/remove.js
@@ -1,11 +1,17 @@
 import { bkInfoBox, bkMessage } from 'bk-magic-vue'
 import LC from '../../index'
 
+let bkInfoInstance = null
+
 export const remove = () => {
+    if (bkInfoInstance !== null) return false
     const activeNode = LC.getActiveNode()
+    if (!activeNode) {
+        return false
+    }
     const parentNode = activeNode.parentNode
     if (!parentNode) {
-        return
+        return false
     }
     let msg = ''
     if (!parentNode.layoutType) {
@@ -20,7 +26,7 @@ export const remove = () => {
             msg = '列数至少为 1 列'
         }
     }
-    
+
     if (msg) {
         bkMessage({
             theme: 'warning',
@@ -29,12 +35,32 @@ export const remove = () => {
         })
         return false
     }
+    // infobox弹出后监听enter事件
+    window.addEventListener('keydown', keyDownCallback)
+
     const { name, componentId } = activeNode
-    bkInfoBox({
+    bkInfoInstance = bkInfoBox({
         title: '删除',
         subTitle: `确认删除 ${name}(${componentId}) 组件？`,
         confirmFn: () => {
             parentNode.removeChild(activeNode)
+            window.removeEventListener('keydown', keyDownCallback)
+            bkInfoInstance = null
+        },
+        cancelFn: () => {
+            window.removeEventListener('keydown', keyDownCallback)
+            bkInfoInstance = null
         }
     })
+}
+
+function keyDownCallback (event) {
+    if (event.keyCode === 13 && bkInfoInstance) {
+        const activeNode = LC.getActiveNode()
+        const parentNode = activeNode.parentNode
+        parentNode.removeChild(activeNode)
+        window.removeEventListener('keydown', keyDownCallback)
+        bkInfoInstance.close()
+        bkInfoInstance = null
+    }
 }
