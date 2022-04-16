5fa6a534636761c5f9e9372e05bcc7314301214a
terlinhe <terlinhe@tencent.com>
2022-04-16 18:59:20 +0800

    fix: fix模板函数变量问题

--- paas-ce/lesscode/lib/client/src/views/project/template-manage/components/template-mixin.js
+++ paas-ce/lesscode/lib/client/src/views/project/template-manage/components/template-mixin.js
@@ -4,17 +4,10 @@ import LC from '@/element-materials/core'
 
 export default {
     methods: {
-        ...mapActions('functions', [
-            'getAllGroupAndFunction'
-        ]),
-        ...mapActions('variable', ['getAllVariable', 'getFunctionVariable']),
+        ...mapActions('pageTemplate', ['getProjectFuncAndVar']),
 
         async getVarAndFuncList (template) {
-            // 将模板中用到的函数和变量都获取出来
-            const [variableList, funcGroups] = await Promise.all([
-                this.getAllVariable({ projectId: template.belongProjectId, versionId: template.versionId, pageCode: template.fromPageCode, effectiveRange: 0 }, false),
-                this.getAllGroupAndFunction({ projectId: template.belongProjectId, versionId: template.versionId })
-            ])
+            const { variableList, funcGroups } = await this.getProjectFuncAndVar({ projectId: template.belongProjectId, versionId: template.versionId, pageCode: template.fromPageCode })
             const templateNode = LC.parseTemplate(JSON.parse(template.content || {}))
             const result = parseFuncAndVar(templateNode, variableList, funcGroups)
             return result
