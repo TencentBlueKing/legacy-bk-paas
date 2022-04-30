5c9005a03074bf210fe1b9463828ad97f4f7dc3d
luofann <fannluowork@gmail.com>
2022-04-30 17:49:46 +0800

    minor: form组件使用demo

--- paas-ce/lesscode/lib/client/src/components/render-nocode/form/components/form-content/index.vue
+++ paas-ce/lesscode/lib/client/src/components/render-nocode/form/components/form-content/index.vue
@@ -1,11 +1,31 @@
 <template>
-    <div>render-form</div>
+    <div>
+        <h3>form组件整个引用：</h3>
+        <hr />
+        <process-form-plugin :fields="formFields"></process-form-plugin>
+        <br />
+        <br />
+        <h3>form组件单个field引用：</h3>
+        <hr />
+        <process-field-item :field="formFields[0]"></process-field-item>
+        <br />
+    </div>
 </template>
 
 <script>
+    import Vue from 'vue'
+    import { ProcessFormPlugin, ProcessFieldItem } from '../../../common/processFormPlugin.js'
+    import '../../../common/processFormPlugin.css'
+    import jsonData from '../../../common/mockFormData.json'
+    console.log(jsonData)
+
+    Vue.use(ProcessFormPlugin)
+    Vue.use(ProcessFieldItem)
     export default {
         data () {
-            return {}
+            return {
+                formFields: jsonData
+            }
         }
     }
 </script>
