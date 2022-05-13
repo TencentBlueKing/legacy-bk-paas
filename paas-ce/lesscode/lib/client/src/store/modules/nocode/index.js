193ffadfe3b7f861256aa0646251155695e6af65
luofann <fannluowork@gmail.com>
2022-05-13 19:31:21 +0800

    merge branch

--- paas-ce/lesscode/lib/client/src/store/modules/nocode/index.js
+++ paas-ce/lesscode/lib/client/src/store/modules/nocode/index.js
@@ -1,12 +1,14 @@
 import form from './form.js'
 import flow from './flow.js'
 import dataSource from './data-source.js'
+import formSetting from './form-setting'
 
 export default {
     namespaced: true,
     modules: {
         form,
         flow,
-        dataSource
+        dataSource,
+        formSetting
     }
 }
