8ae273113be7e89f7a92caab8e0a6e5c1b4dd926
hLinx <327159425@qq.com>
2022-05-18 14:49:56 +0800

    perf: 画布优化

--- paas-ce/lesscode/lib/client/src/element-materials/core/extends/set-event.js
+++ paas-ce/lesscode/lib/client/src/element-materials/core/extends/set-event.js
@@ -7,23 +7,28 @@
  *
  * - params1 是 Object 时 params2 无效
  * - params1 是 String 时表示属性名，params2 为对应的属性值
- *
- * ep.
- *
- * 1,
- * node.setEvent('click', {
- *  methodCode: '',
- *  params: []
- * })
- *
- * 2,
- * node.setEvent({
- *  click: {
- *    methodCode: '',
- *    params: []
- *  }
- * })
  */
+
+/* eq 1,
+
+ node.setEvent('click', {
+  methodCode: '',
+  params: []
+ })
+
+*/
+
+/* ep 2,
+
+ node.setEvent({
+  click: {
+    methodCode: '',
+    params: []
+  }
+ })
+ 
+*/
+
 export default function (node, params1, params2 = '') {
     let eventData = {}
     if (Object.prototype.toString.call(params1) === '[object String]') {
